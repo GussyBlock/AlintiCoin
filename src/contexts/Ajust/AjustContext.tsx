108b5ade4198d3c9b472c0791a4dcec7127a72e3
Gussy Block <gussyblockchain@gmail.com>
2022-11-21 14:54:54 -0500

    Se elimino carpetas inservibles

--- src/contexts/Ajust/AjustContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { AjustContextProps } from "./interfaces";
-
-export const AjustContext = createContext<AjustContextProps>(
-  {} as AjustContextProps
-);
--- src/contexts/Preloader/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface IProps {
-	children: JSX.Element | JSX.Element[] 
-}
-
-export interface IPreloader {
-	preloader: boolean,
-}
